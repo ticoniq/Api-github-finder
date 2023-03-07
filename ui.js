f8d4e0e1870bc1f8f446ebdf66f58fbaac2ea866
ticoniq_ <okolietochi@gmail>
2023-03-07 08:00:41 +0100

    fetch and display github user data

--- ui.js
+++ ui.js
@@ -16,8 +16,8 @@ class UI {
           
           <div class="col-md-9">
             <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-Secondary">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-Success">Followers: ${user.followers}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
             <span class="badge badge-info">Following: ${user.following}</span>
             <br><br>
             <ul class="list-group>
@@ -32,6 +32,27 @@ class UI {
       <h3 class="page heading mb-3">Latest Repos </h3>
       <div id="repos"></div>
     `;
+  }
+
+  showRepos(repos) {
+    let output = '';
+    repos.forEach(repo => {
+      output +=`<div class="card card-body mb-2>
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-secondary">Stars: ${repos.stargazers_count}</span>
+            <span class="badge badge-success">Watcher: ${repo.watchers_count}</span>
+            <span class="badge badge-info">Forks: ${repo.forms_count}</span>
+          </div>
+        </div>
+      </div>`;
+    });
+
+    // Output repos 
+    document.getElementById('repos').innerHTML = output;
   }
 
   // Show alert message
